1efa02211fed17c9ae0db8ca47830c35d4e81655
dipendra-react <66115431+Dipendra-creator@users.noreply.github.com>
2022-04-14 16:29:37 +0530

    ✅404 Section Modifications🪢

---
+++ pages/404.jsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+
+export default function Custom404() {
+  return (
+    <div className="center" >
+      <div className="center container">
+      <div className='error-section'>
+        <div style={{'text-align': 'center'}}>
+          <h1 className='error-head'><span className='error'>{"{404}"}</span> Whoops! This Page Doesn't Exist...</h1>
+          <p className='error-text'>Not to worry. You can headover to Our <span>HomePage</span> or <span> you can holler at us on our Mail.</span></p>
+        </div>
+        <div className="center">
+        <iframe
+          src="https://giphy.com/embed/l2JehQ2GitHGdVG9y"
+          width="375"
+          height="362"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+        </div>
+        <div className="center">
+          <Link href="/">
+            <button className="btn-blue">Go home</button>
+          </Link>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+}
